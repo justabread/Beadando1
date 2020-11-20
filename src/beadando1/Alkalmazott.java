ac9aaa556c866760febb83c7e56954be81373cc1
Eszti400 <74489565+Eszti400@users.noreply.github.com>
2020-11-20 16:01:04 +0100

    Create Alkalmazott.java

---
+++ src/beadando1/Alkalmazott.java
@@ -0,0 +1,15 @@
+package beadando1;
+
+public class Alkalmazott {
+    private final int employeeId;
+
+    public Alkalmazott(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    
+    public void manageCar(){}
+    
+    public void contact(){}
+    
+    public void listRents(){}
+}
